af31df58d1ac931f9cd1f3fbff230f4e6e4dda41
jazielloureiro <jazielloureiro17@gmail.com>
2022-04-02 10:34:14 -0300

    Add get_exchange_rates function

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,1 +1,11 @@
-print('Hello, World!')
+import requests
+
+def get_exchange_rates(currency, date):
+    api_url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda=\'{}\'&@dataCotacao=\'{}\'&$top=10&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao,tipoBoletim'
+
+    response = requests.get(api_url.format(currency, date))
+
+    return response.json()['value']
+
+print(get_exchange_rates('USD', '04-01-2022'))
+print(get_exchange_rates('USD', '04-02-2022'))
