91227a28dfed156f8bb3c286a12e3858510e598b
jazielloureiro <jazielloureiro17@gmail.com>
2022-04-02 16:24:44 -0300

    Add start command

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,5 +1,21 @@
 import requests
 import datetime as dt
+import telebot as tb
+
+api_token = ''
+bot = tb.TeleBot(api_token)
+
+currencies = {}
+currencies['DKK'] = {'Unicode': '\U0001F1E9\U0001F1F0', 'Name': 'Coroa Dinamarquesa'}
+currencies['NOK'] = {'Unicode': '\U0001F1F3\U0001F1F4', 'Name': 'Coroa Norueguesa'}
+currencies['SEK'] = {'Unicode': '\U0001F1F8\U0001F1EA', 'Name': 'Coroa Sueca'}
+currencies['USD'] = {'Unicode': '\U0001F1FA\U0001F1F8', 'Name': 'Dólar Americano'}
+currencies['AUD'] = {'Unicode': '\U0001F1E6\U0001F1FA', 'Name': 'Dólar Australiano'}
+currencies['CAD'] = {'Unicode': '\U0001F1E8\U0001F1E6', 'Name': 'Dólar Canadense'}
+currencies['EUR'] = {'Unicode': '\U0001F1EA\U0001F1FA', 'Name': 'Euro'}
+currencies['CHF'] = {'Unicode': '\U0001F1E8\U0001F1ED', 'Name': 'Franco Suíço'}
+currencies['JPY'] = {'Unicode': '\U0001F1EF\U0001F1F5', 'Name': 'Iene'}
+currencies['GBP'] = {'Unicode': '\U0001F1EC\U0001F1E7', 'Name': 'Libra Esterlina'}
 
 def get_exchange_rates(currency, date):
     api_url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda=\'{}\'&@dataCotacao=\'{}\'&$top=10&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao,tipoBoletim'
@@ -43,19 +59,14 @@ def format_report(report, currency):
 
     return str_report
 
-currencies = {}
-currencies['DKK'] = {'Unicode': '\U0001F1E9\U0001F1EF', 'Name': 'Coroa Dinamarquesa'}
-currencies['NOK'] = {'Unicode': '\U0001F1F3\U0001F1F4', 'Name': 'Coroa Norueguesa'}
-currencies['SEK'] = {'Unicode': '\U0001F1F8\U0001F1EA', 'Name': 'Coroa Sueca'}
-currencies['USD'] = {'Unicode': '\U0001F1FA\U0001F1F8', 'Name': 'Dólar Americano'}
-currencies['AUD'] = {'Unicode': '\U0001F1E6\U0001F1FA', 'Name': 'Dólar Australiano'}
-currencies['CAD'] = {'Unicode': '\U0001F1E8\U0001F1E6', 'Name': 'Dólar Canadense'}
-currencies['EUR'] = {'Unicode': '\U0001F1EA\U0001F1FA', 'Name': 'Euro'}
-currencies['CHF'] = {'Unicode': '\U0001F1E8\U0001F1ED', 'Name': 'Franco Suíço'}
-currencies['JPY'] = {'Unicode': '\U0001F1EF\U0001F1F5', 'Name': 'Iene'}
-currencies['GBP'] = {'Unicode': '\U0001F1EC\U0001F1E7', 'Name': 'Libra Esterlina'}
+@bot.message_handler(commands=['start'])
+def show_start_message(message):
+    keyboard = tb.types.ReplyKeyboardMarkup()
 
-for i in currencies.keys():
-    report = get_latest_report(i)
-    print(format_report(report, i))
-    print()
+    for i in currencies.keys():
+        button_label = '{0} {1} ({2}) {0}\n'.format(currencies[i]['Unicode'], currencies[i]['Name'], i)
+        keyboard.row(tb.types.KeyboardButton(button_label))
+
+    bot.send_message(message.chat.id, 'Hello', reply_markup=keyboard)
+
+bot.infinity_polling()
