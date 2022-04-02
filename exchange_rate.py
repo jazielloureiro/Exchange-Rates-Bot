492fd2f3f3d5ef3edb886975687680a31f197a9d
jazielloureiro <jazielloureiro17@gmail.com>
2022-04-02 10:58:27 -0300

    Add get_latest_report function

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,4 +1,5 @@
 import requests
+import datetime as dt
 
 def get_exchange_rates(currency, date):
     api_url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda=\'{}\'&@dataCotacao=\'{}\'&$top=10&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao,tipoBoletim'
@@ -7,5 +8,15 @@ def get_exchange_rates(currency, date):
 
     return response.json()['value']
 
-print(get_exchange_rates('USD', '04-01-2022'))
-print(get_exchange_rates('USD', '04-02-2022'))
+def get_latest_report(currency):
+    date = dt.date.today()
+    rates = get_exchange_rates(currency, date.strftime('%m-%d-%Y'))
+
+    while not rates:
+        date -= dt.timedelta(days=1)
+        rates = get_exchange_rates(currency, date.strftime('%m-%d-%Y'))
+
+    return rates
+
+print(get_latest_report('USD'))
+print(get_latest_report('EUR'))
